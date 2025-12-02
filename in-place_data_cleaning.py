abe8042b3a8924676dc361b8a5f7469c8e02cb19
Durdona <durdonayuldasheva989@gmail.com>
2025-12-02 16:14:42 +0500

    week 5

---
+++ in-place_data_cleaning.py
@@ -0,0 +1,16 @@
+def remove_outliers(data, min_val, max_val):
+    i = 0  
+    while i < len(data):
+        if data[i] < min_val or data[i] > max_val:
+            data.remove(data[i])
+            i+= 1
+measurements = [8, 5, 12, 1, 9, 20, 15]
+print(f"Original measurements: {measurements}")
+cleaned_measurement = remove_outliers(measurements, 5, 15)
+print(f"Cleaned measurements: {cleaned_measurement}")
+temps = [-5, 10, 0, 35, 15, 40, 20]
+print(f"Original temps: {temps}")
+remove_outliers(temps, 0, 30)
+edge_case = [100, 1, 101, 2, 102]
+print(f"Original edge case: {edge_case}")
+remove_outliers(edge_case, 0, 10)
