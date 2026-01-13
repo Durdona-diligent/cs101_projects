cd415f917cc7be96ce828d4f9959e75cd59b10a8
Durdona <durdonayuldasheva989@gmail.com>
2026-01-13 12:50:00 +0500

    week 15

--- problem1.py
+++ problem1.py
@@ -1,21 +1,18 @@
-#The Ingredient Checker
-def check_ingredients(pantry_list, recipe_list):
-    lower_pantry = [item.lower() for item in pantry_list]
-    lower_recipe = [item.lower() for item in recipe_list]
-
-    pantry_set = set(lower_pantry)
-    recipe_set = set(lower_recipe)
-    missing_items = sorted(recipe_set - pantry_set)
-    available_items = sorted(pantry_set & recipe_set)
-    
-    missing = list(missing_items)
-    available = list(available_items)
-    
-    return missing, available
-pantry = ["Eggs", "flour", "Milk", "eggs", "salt"]
-recipe = ["flour", "milk", "Sugar", "Eggs", "butter"]
-
-missing, available = check_ingredients(pantry, recipe)
-
-print(f"Need to buy: {missing}")
-print(f"Already have: {available}")
+# The Password Strength 
+def validate_password(password):
+    if len(password) >= 8:
+        if password != password.lower():
+            if "password" not in password.lower():
+                for char in password:
+                    if '1' <= char <= '9':
+                        return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+print(validate_password("apple"))           # False (too short)
+print(validate_password("Password123"))     # False (contains 'password')
+print(validate_password("security"))        # False (no uppercase, no digit)
+print(validate_password("SecureCode99"))    # True
